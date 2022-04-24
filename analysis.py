567bd58cb4ad1fd95d05a687d1120026d1b04a27
Jasper Paterson <jasperpato@gmail.com>
2022-04-24 16:03:42 +0800

    seq resets untested

--- analysis.py
+++ analysis.py
@@ -14,6 +14,7 @@ def report_dropped(filename):
   prev_coords = ()
   prev_seq = -1
   dropped = []
+  seq_resets = 0
 
   for i, l in enumerate(f.readlines()):
     
@@ -28,10 +29,15 @@ def report_dropped(filename):
       seq = int(d[1])
     except: continue
 
+    if seq < prev_seq: # assume reset
+      seq_resets += prev_seq - seq
+
+    seq += seq_resets
+
     if i > 0 and seq > prev_seq + 1:
       # print(f"Dropped {seq - prev_seq - 1} packet(s) between {prev_seq} and {seq}, {prev_coords} and {coords}.\n")
       dropped.append(((prev_seq, prev_coords), (seq, coords)))
-    
+
     prev_seq = seq
     prev_coords = coords
 
